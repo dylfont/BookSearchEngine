997e8f71155c9b4f2eb2e7ee70eb3c6ca14dfd18
Dylan <dylanfontenot@protonmail.com>
2021-11-21 20:32:44 -0600

    added mutations and queries, edited save and search book functi onality, and fixed login, navbar, and signup.

--- Develop/client/src/utils/auth.js
+++ Develop/client/src/utils/auth.js
@@ -17,15 +17,15 @@ class AuthService {
 
   // check if token is expired
   isTokenExpired(token) {
-    try {
+   
       const decoded = decode(token);
       if (decoded.exp < Date.now() / 1000) {
+       localStorage.removeItem('id_token');
         return true;
-      } else return false;
-    } catch (err) {
+      } 
       return false;
-    }
-  }
+    } 
+  
 
   getToken() {
     // Retrieves the user token from localStorage
